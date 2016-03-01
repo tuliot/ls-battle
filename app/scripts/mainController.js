6f17010dd08b9bffc84389bed6bc53be17b6aacb
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2016-03-01 18:49:51 -0500

    Added some basic styling and creating http functionality
    Added some basic styling and progress bars. Added the function for http, waiting on dummy data. Also started a function for openVoting

--- app/scripts/mainController.js
+++ app/scripts/mainController.js
@@ -3,5 +3,17 @@ angular.module('ls_battle')
 .controller('MainController', mainCtrl);
 
 function mainCtrl($http){
-  
+
+  var self = this;
+
+  $http.get('').success(function(data){
+
+    console.log(data)
+
+  })
+
+  self.openVoting = function(){
+    
+  }
+
 }
