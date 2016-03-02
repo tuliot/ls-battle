4845ebe66706e33d426198aa36477b43c05756f6
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2016-03-01 21:03:02 -0500

    Set interval function, data refreshes now

--- app/scripts/mainController.js
+++ app/scripts/mainController.js
@@ -2,26 +2,33 @@
 angular.module('ls_battle')
 .controller('MainController', mainCtrl);
 
-function mainCtrl($http){
+function mainCtrl($http,$timeout,$scope){
+
+
 
   var self = this;
 
-  $http.get('../data.json').success(function(data){
-    self.teams = data.contestants;
-    self.votes0 = data.contestants[0].voteCount;
-    self.votes1 = data.contestants[1].voteCount;
-    self.votes2 = data.contestants[2].voteCount;
-    self.votes3 = data.contestants[3].voteCount;
+  self.grabData = function(){
+    console.log("gettingData")
+    $http.get('../data.json').success(function(data){
+      self.teams = data.contestants;
+      self.votes0 = data.contestants[0].voteCount;
+      self.votes1 = data.contestants[1].voteCount;
+      self.votes2 = data.contestants[2].voteCount;
+      self.votes3 = data.contestants[3].voteCount;
+      console.log(data)
+    })
 
-    console.log(data)
+    $http.get('../data.json').success(function(data){
+      self.tweets = data.tweets;
+      console.log(data)
+    })
+  }
 
-  })
+      setInterval(self.grabData,1000);
 
-  $http.get('../data.json').success(function(data){
 
-    self.tweets = data.tweets;
-    console.log(data)
 
-  })
+
 
 }
