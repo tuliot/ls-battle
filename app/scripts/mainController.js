aadd1fb71339eca8be2912ce41b0cf0eb69f5890
Tulio Troncoso <tulio@wearespry.com>
2016-03-03 23:43:24 -0500

    Made the addition of users dynamic Progress bars and User pictures are NOT hardcoded anymore Added progressBar.html, but this is not being used right now

--- app/scripts/mainController.js
+++ app/scripts/mainController.js
@@ -4,43 +4,22 @@ angular.module('ls_battle')
 
 function mainCtrl($http,$timeout,$scope){
 
-
-
   var self = this;
 
-  self.grabData = function(){
-    console.log("gettingData")
+  self.grabData = function() {
+    console.log("Getting Data")
     $http.get('http://localhost:8080/api/v1/results').success(function(data){
       self.teams = data.contestants;
+      self.tweets = data.tweets;
 
-       self.votes = [];
-       data.contestants.forEach(function(contestant){
-       self.votes.push(contestant.voteCount);
-       })
-      // self.votes1 = data.contestants[1].voteCount;
-      // self.votes2 = data.contestants[2].voteCount;
-      // self.votes3 = data.contestants[3].voteCount;
-      console.log('from grab data', data)
-    })
-
-    $http.get('http://localhost:8080/api/v1/results').success(function(data){
-      // self.tweets = data.tweets;
-      console.log('from tweets', data.tweets)
+      console.log('Teams: ', self.teams);
+      console.log('Tweets: ', self.tweets);
     })
 
     $http.get('../data.json').success(function(data){
       self.pics = data.contestants[0].pic;
-      self.tweets = data.tweets;
-      // console.log(data)
     })
-
-
   }
 
-      setInterval(self.grabData,1000);
-
-
-
-
-
+  setInterval(self.grabData,1000);
 }
